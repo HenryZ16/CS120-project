4faf84041131c080416cf39757c746e1d3074d07
HenryZ16 <113991773+HenryZ16@users.noreply.github.com>
2024-08-10 15:10:52 +0800

    ASIO build test

---
+++ src/build_test.rs
@@ -0,0 +1,4 @@
+// To test whether you can successfully build this project.
+pub fn build_test() {
+    println!("build-test: build successfully!");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+// ASIO needed
+#![allow(unused_imports)]
+use anyhow::{Error, Result};
+use cpal::{
+    traits::{DeviceTrait, HostTrait, StreamTrait},
+    Device, FromSample, HostId, Sample, SampleFormat, SampleRate, SizedSample,
+    SupportedStreamConfig,
+};
+use futures::{FutureExt, Sink, SinkExt, Stream, StreamExt};
+use hound::{WavSpec, WavWriter};
+use rodio::{Decoder, OutputStream, Source};
+use std::{
+    fs::File,
+    io::BufWriter,
+    iter::ExactSizeIterator,
+    sync::{Arc, Mutex},
+    thread,
+    time::Duration,
+};
+use tokio::{
+    sync::{
+        mpsc::{self, UnboundedReceiver, UnboundedSender},
+        oneshot::{self, Receiver, Sender},
+    },
+    task, time,
+};
+
+mod build_test;
+use build_test::build_test;
+
 fn main() {
-    println!("Hello, world!");
+    build_test();
 }
