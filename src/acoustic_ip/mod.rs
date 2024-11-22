70c16192a985cbc623083d3d47df0bd5abd8ae67
HenryZ16 <113991773+HenryZ16@users.noreply.github.com>
2024-11-22 12:23:36 +0800

    add gateway

--- src/acoustic_ip/mod.rs
+++ src/acoustic_ip/mod.rs
@@ -9,13 +9,18 @@ async fn test_grab_packet() {
     use std::sync::Arc;
     let ip_addr = Ipv4Addr::new(172, 18, 3, 3);
     let ip_mask = Ipv4Addr::new(255, 255, 0, 0);
+    let ip_gateway = Ipv4Addr::new(172, 18, 0, 1);
 
     let wintun = unsafe { wintun::load_from_path("external\\wintun\\bin\\amd64\\wintun.dll") }
         .expect("Failed to load wintun dll");
-    let adapter = wintun::Adapter::create(&wintun, "AcousticNet", "Wintun", None)
-        .expect("Failed to create wintun adapter!");
+    let adapter = match wintun::Adapter::open(&wintun, "AcousticNet") {
+        Ok(adapter) => adapter,
+        Err(_) => wintun::Adapter::create(&wintun, "AcousticNet", "Wintun", None)
+            .expect("Failed to create wintun adapter!"),
+    };
     adapter.set_address(ip_addr).unwrap();
     adapter.set_netmask(ip_mask).unwrap();
+    adapter.set_gateway(Some(ip_gateway)).unwrap();
 
     let session = Arc::new(adapter.start_session(wintun::MAX_RING_CAPACITY).unwrap());
 
@@ -28,6 +33,17 @@ async fn test_grab_packet() {
             && packet_bytes[19] == 250
         {
             continue;
+        } else if packet_bytes[16] == 1
+            && packet_bytes[17] == 1
+            && packet_bytes[18] == 1
+            && packet_bytes[19] == 1
+        {
+            let mut return_packet = wlan_session
+                .allocate_send_packet(packet_bytes.len() as u16)
+                .unwrap();
+            let return_bytes = return_packet.bytes_mut();
+            return_bytes.copy_from_slice(&packet_bytes);
+            wlan_session.send_packet(return_packet);
         }
         println!(
             "Length: {:?},Received packet: {:?}",
@@ -45,6 +61,7 @@ async fn test_ping() {
     use std::sync::Arc;
     let ip_addr = Ipv4Addr::new(172, 18, 3, 3);
     let ip_mask = Ipv4Addr::new(255, 255, 0, 0);
+    let ip_gateway = Ipv4Addr::new(172, 18, 0, 1);
 
     let wintun = unsafe { wintun::load_from_path("external\\wintun\\bin\\amd64\\wintun.dll") }
         .expect("Failed to load wintun dll");
@@ -52,6 +69,7 @@ async fn test_ping() {
         .expect("Failed to create wintun adapter!");
     adapter.set_address(ip_addr).unwrap();
     adapter.set_netmask(ip_mask).unwrap();
+    adapter.set_gateway(Some(ip_gateway)).unwrap();
 
     let session = Arc::new(adapter.start_session(wintun::MAX_RING_CAPACITY).unwrap());
 
