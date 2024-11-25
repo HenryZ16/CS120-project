39ba086cfc5785615c3b55f1df92163b03d00ab6
HenryZ16 <113991773+HenryZ16@users.noreply.github.com>
2024-11-25 19:45:25 +0800

    design of adapter

--- src/acoustic_ip/adapter.rs
+++ src/acoustic_ip/adapter.rs
@@ -4,6 +4,8 @@ use std::net::Ipv4Addr;
 use std::sync::Arc;
 use wintun::{Packet, Session};
 
+use super::ip_packet::IpPacket;
+
 pub struct Adapter {
     // to IP layer
     ip_addr: Ipv4Addr,
@@ -56,11 +58,48 @@ impl Adapter {
         )
     }
 
+    pub fn send_to_ip(&self, packet: IpPacket) {
+        let packet_bytes = packet.get_ip_packet_bytes();
+        let mut return_packet = self
+            .session
+            .allocate_send_packet(packet_bytes.len() as u16)
+            .unwrap();
+        let return_bytes = return_packet.bytes_mut();
+        return_bytes.copy_from_slice(&packet_bytes);
+        self.session.send_packet(return_packet);
+    }
+
+    pub fn receive_from_ip_blocking(&self) -> Result<IpPacket, &'static str> {
+        let packet = self.session.receive_blocking().unwrap();
+        let packet_bytes = packet.bytes().to_vec();
+        if packet_bytes[0] >> 4 != 4 {
+            Err("Only IPv4 is supported")?;
+        }
+        Ok(IpPacket::new_from_bytes(&packet_bytes))
+    }
+
+    pub fn receive_from_ip_async(&self) -> Result<IpPacket, &'static str> {
+        match self.session.try_receive() {
+            Ok(Some(packet)) => {
+                let packet_bytes = packet.bytes().to_vec();
+                if packet_bytes[0] >> 4 != 4 {
+                    Err("Only IPv4 is supported")?;
+                }
+                Ok(IpPacket::new_from_bytes(&packet_bytes))
+            }
+            Ok(None) => Err("No packet received")?,
+            Err(_) => Err("Failed to receive packet")?,
+        }
+    }
+
+    async fn up_daemon(&self) {}
+    async fn down_daemon(&self) {}
+
     pub async fn adapter_daemon(&self) {
-        // 1. Listen from the mac layer
+        // 1. Listen from the mac layer (up)
         //    if `ping` echoRequest, send `ping` echoReply
         //    else, send the packet to the ip layer
-        // 2. Listen from the ip layer
+        // 2. Listen from the ip layer (down)
         //    if is router, and the subnet of the packet is for the acoustic network,
         //    send the packet to the mac layer
         //    else, do nothing
