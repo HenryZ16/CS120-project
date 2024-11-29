b98a479300451866ab7fb41a62ad4142359b1564
HenryZ16 <113991773+HenryZ16@users.noreply.github.com>
2024-11-29 21:59:24 +0800

    rm `== true`

--- src/acoustic_ip/adapter.rs
+++ src/acoustic_ip/adapter.rs
@@ -283,7 +283,7 @@ impl Adapter {
     pub async fn start_daemon(mut adapter: Adapter) -> tokio::task::JoinHandle<()> {
         let (to_out_tx, to_out_rx) = unbounded_channel();
         let (get_in_tx, get_in_rx) = unbounded_channel();
-        if adapter.if_router == true {
+        if adapter.if_router {
             nat_forward_daemon(
                 to_out_rx,
                 get_in_tx,
